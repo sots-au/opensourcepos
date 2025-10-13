8e6e37011fbf23f2a52a931bf237e84184d4766c
Amit Kumar <amit21thakur@gmail.com>
2025-10-13 23:31:36 +1100

    Update nginx reload and sed usage in startup.sh
    Replaces 'service nginx reload' with 'nginx -s reload' for reloading nginx. Updates sed commands to use --in-place and --follow-symlinks for safer file editing. Adds TMPDIR variable assignment.

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp /etc/nginx/sites-available/default /home/site/default
-sed -i 's|root /home/site/wwwroot;|root /home/site/wwwroot/public;|g' /home/site/default
-sed -i 's|index index.html index.htm;|index index.php index.html index.htm;|g' /home/site/default
-service nginx reload
-
+TMPDIR=/tmp
+sed --in-place --follow-symlinks 's|root /home/site/wwwroot;|root /home/site/wwwroot/public;|g' /home/site/default
+sed --in-place --follow-symlinks 's|index index.html index.htm;|index index.php index.html index.htm;|g' /home/site/default
+nginx -s reload
